c44fd7755fa0d1eb8c28e4c67c7d1d83f57772d3
murphyAk <65244819+murphyAk@users.noreply.github.com>
2020-06-16 12:04:49 -0400

    finished

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace csharp_web_dev_lsn9exceptions
 {
@@ -7,18 +8,45 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            return x / y;
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if (fileName.EndsWith("cs"))
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
 
         static void Main(string[] args)
         {
-            // Test out your Divide() function here!
+            Console.Write("Enter overall score: ");
+            double totalScore = double.Parse(Console.ReadLine());
+            Console.Write("Enter student score: ");
+            double studentScore = double.Parse(Console.ReadLine());
+
+            try
+            {
+                double grade = Divide(studentScore, totalScore);
+
+                if (totalScore == 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine($"Score is {Divide(studentScore, totalScore) * 100}");
+
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("you cannot divide by zero!");
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -26,6 +54,23 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+
+            foreach (KeyValuePair<string, string> submissions in students)
+            {
+                try
+                {
+                    if (submissions.Value == null || submissions.Value == "")
+                    {
+                        throw new NullReferenceException();
+                    }
+                    Console.WriteLine(CheckFileExtension(submissions.Value));
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Invalid file format");
+                }
+            }
+
 
 
         }
